bb08b229c487b41233e73c1480fe415fe3714910
mast3r <thomasmollerjensen@live.dk>
2024-03-25 11:19:40 +0100

    implemented changes from comments

--- tests/test_api.py
+++ tests/test_api.py
@@ -3,24 +3,31 @@ import sys
 sys.path.append(f'{os.path.dirname(__file__)}/..')
 
 import pytest
+from json import loads
 
 import modules.api as api
+
+# load test data
+with open("files/testdata/default.json", "r") as file:
+    data = file.read()
+    default_questions:list[str] = loads(data)["questions"]
+    users:dict[str, dict] = loads(data)["users"]
 
 # authorization
 
 @pytest.mark.skip(reason="Not implemented")
 def test_register():
     API = api.API()
-    username = "tester"
-    password = "tester"
+    username = users.keys()[0]
+    password = users[username]["password"]
     result = API.register(username, password)
     assert result
 
 @pytest.mark.skip(reason="Not implemented")
 def test_login():
     API = api.API()
-    username = "tester"
-    password = "tester"
+    username = users.keys()[0]
+    password = users[username]["password"]
     API.register(username, password)
     token = API.login(username, password)
     assert not token == ""
@@ -28,14 +35,10 @@ def test_login():
 @pytest.mark.skip(reason="Not implemented")
 def test_get_initial_questions():
     API = api.API()
-    username = "tester"
-    password = "tester"
+    username = users.keys()[0]
+    password = users[username]["password"]
     API.register(username, password)
     token = API.login(username, password)
-    assert API.get_initial(token) == [
-        "How old are you?",
-        "What is your occupation?",
-        "Do you have children?" # ... fill in later
-    ]
+    assert API.get_initial(token) == default_questions
 
 
--- tests/test_database.py
+++ tests/test_database.py
@@ -3,8 +3,15 @@ import sys
 sys.path.append(f'{os.path.dirname(__file__)}/..')
 
 import pytest
+from json import loads
 
 import modules.database as database
+
+# load test data
+with open("files/testdata/default.json", "r") as file:
+    data = file.read()
+    default_questions = loads(data)["questions"]
+    users:dict[str, dict] = loads(data)["users"]
 
 # users table tests
 
@@ -12,22 +19,24 @@ import modules.database as database
 def test_add_user():
     num_users = 1000
     for i in range(num_users):
-        database.users.add(f'user{i}')
+        database.users.add(f'user{i}', passwd="test")
     
     assert len(database.users.all()) == num_users
 
 @pytest.mark.skip(reason="Not implemented")
 def test_delete_user():
-    database.users.add("user")
-    user_id = database.users.all()[0]
-    assert len(database.users.all()) == 1
+    username = users.keys()[0]
+    password = users[username]["password"]
+    user_id = database.users.add(username, passwd=password)
+    assert database.users.size() == 1
     database.users.delete(user_id)
-    assert len(database.users.all()) == 0
+    assert database.users.size() == 0
 
 @pytest.mark.skip(reason="Not implemented")
 def test_modify_user():
-    database.users.add("user")
-    user_id = database.users.all()[0]
+    username = users.keys()[0]
+    password = users[username]["password"]
+    user_id = database.users.add(username, passwd=password)
     current = database.users.getJSON(user_id)
     database.users.modify(user_id, name="user1")
     new = database.users.getJSON(user_id)
@@ -36,8 +45,9 @@ def test_modify_user():
 # journals table tests
 @pytest.mark.skip(reason="Not implemented")
 def test_create_journal():
-    database.users.add("user")
-    user_id = database.users.all()[0]
+    username = users.keys()[0]
+    password = users[username]["password"]
+    user_id = database.users.add(username, passwd=password)
     num_journals = 1000
     database.journals.add(user_id, [(f"q{i}", f"a{i}") for i in range(num_journals)])
     assert len(database.journals.all()) == num_journals
