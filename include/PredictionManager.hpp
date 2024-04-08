272fb06e792b307cffbf1e1238ccf1bebc7b69b8
mast3r <thomasmollerjensen@live.dk>
2024-04-08 10:10:07 +0200

    predictions implementation

--- include/PredictionManager.hpp
+++ include/PredictionManager.hpp
@@ -1,20 +1,32 @@
 #include <cstddef>
 #include <string>
+#include <vector>
+
+#define LOW_STRESS 0
+#define MODERATE_STRESS 14
+#define HIGH_STRESS 27
 
 class PredictionBuilder {
-    float prediction_value = 0.0;
-    size_t _size = 0;
+    float prediction_value = 0.5;
+    std::vector<std::pair<std::string, int>> valued_data;
+    std::vector<std::pair<std::string, bool>> boolean_data;
     public:
-        void add_valued_data(std::string qid, float data){
-            _size++;
+        void add_valued_data(std::string qid, int data){
+            valued_data.push_back(std::make_pair(qid, data));
         }
         void add_boolean_data(std::string qid, bool data){
-            _size++;
+            boolean_data.push_back(std::make_pair(qid, data));
         }
         size_t size(){
-            return _size;
+            return valued_data.size() + boolean_data.size();
         }
         float build(){
+            for(auto pair : valued_data){
+                prediction_value += (pair.second);
+            }
+            for(auto pair : boolean_data){
+                prediction_value += pair.second ? .1 : -(.1);
+            }
             return prediction_value;
         }
 };
