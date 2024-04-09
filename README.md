275622f2a9a1164ac424e5c531bff7fc264e24ca
mast3r <thomasmollerjensen@live.dk>
2024-04-09 15:08:58 +0200

    work from today

---
+++ README.md
@@ -0,0 +1,7 @@
+# P8 Backend software
+
+INstall: 
+if on nix, run:
+```sh
+nix shell github:cs-24-sw-8-11
+```
--- src/predictions_test.cpp
+++ src/predictions_test.cpp
@@ -13,11 +13,11 @@ class PredictionTest : public Test<std::function<void()>> {
         PredictionManager manager;
 };
 
-template<typename T>
-std::vector<T> make_range(int n, std::function<T(int)> f){
+template<typename T = int>
+std::vector<T> make_range(int n, std::function<T(int)> functor){
     std::vector<T> result;
     for(auto i = 0; i < n; i++){
-        result.push_back(f(i));
+        result.push_back(functor(i));
     }
     return result;
 }
@@ -26,7 +26,7 @@ int main(){
     PredictionTest test;
     test.add_test("add-valued-data", [&](){ 
         auto uid = 1;
-        auto data = make_range<std::pair<std::string, double>>(num_entries, [](int i){return std::make_pair(std::format("{}", i), i/100);});
+        auto data = make_range<std::pair<std::string, double>>(num_entries, [](int i){return std::make_pair(std::format("{}", i), i/num_entries);});
         auto predictionBuilder = test.manager.create_new_prediction(uid);
         for(auto pair : data){
             predictionBuilder.add_valued_data(pair.first, pair.second);
@@ -56,6 +56,22 @@ int main(){
     test.run();
 
     PredictionTest extended;
+
+    extended.add_test("regression test", [&](){
+        auto uid = 1;
+        std::vector<double> results;
+        for(auto i = 0; i < num_entries; i++){
+            auto data = make_range<std::pair<std::string, double>>(10, [](int i){
+                return std::make_pair(std::format("{}", i), i%5);
+            });
+            auto builder = extended.manager.create_new_prediction(uid);
+            for(auto pair : data)
+                builder.add_valued_data(pair.first, pair.second);
+            results.push_back(builder.build());
+        }
+        //auto futureData = extended.manager.create_new_regression_model(uid);
+        
+    });
 
 
 };
