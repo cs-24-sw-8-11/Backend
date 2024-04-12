d7810eee3c2b0c68a7d54aa402375ac5e65255c9
mast3r <thomasmollerjensen@live.dk>
2024-04-12 13:02:58 +0200

    formatted code

--- include/Database.hpp
+++ include/Database.hpp
@@ -1,4 +1,9 @@
+#pragma once
+#include <memory>
+#include <string>
+
 #include "Table.hpp"
+
 
 enum QuestionType {
     VALUED,
@@ -10,61 +15,60 @@ enum UserState {
     STANDARD
 };
 // wrappers to make db access easy while keeping types
-std::string db_int(int e){
+std::string db_int(int e) {
     return std::format("{}", e);
 }
 
 class Database {
-    public:
-        std::shared_ptr<Table> users;
-        std::shared_ptr<Table> journals;
-        std::shared_ptr<Table> answers;
-        std::shared_ptr<Table> questions;
-        std::shared_ptr<Table> settings;
-        std::shared_ptr<Table> userdata;
-        std::shared_ptr<Table> predictions;
-
-        Database(std::string path){
-            TableFactory factory{path};
-            this->users = factory.create("users", {
-                "username VARCHAR UNIQUE NOT NULL",
-                "password VARCHAR NOT NULL",
-                "userdataId INTEGER",
-                "state INTEGER NOT NULL",
-                "FOREIGN KEY(userdataId) REFERENCES userdata(id)"
-            });
-            this->journals = factory.create("journals", {
-                "comment varchar",
-                "userId INTEGER NOT NULL",
-                "FOREIGN KEY(userId) REFERENCES users(id)"
-            });
-            this->answers = factory.create("answers", {
-                "answer VARCHAR NOT NULL",
-                "journalId INTEGER NOT NULL",
-                "questionId INTEGER NOT NULL",
-                "FOREIGN KEY(journalId) REFERENCES journals(id)",
-                "FOREIGN KEY(questionId) REFERENCES questions(id)"
-            });
-            this->questions = factory.create("questions", {
-                "tags VARCHAR",
-                "type INTEGER NOT NULL",
-                "question VARCHAR NOT NULL"
-            });
-            this->settings = factory.create("settings", {
-                "key VARCHAR NOT NULL",
-                "value VARCHAR NOT NULL",
-                "userId INTEGER NOT NULL",
-                "FOREIGN KEY(userId) REFERENCES users(id)"
-            });
-            this->userdata = factory.create("userdata", {
-                "agegroup VARCHAR NOT NULL",
-                "occupation VARCHAR NOT NULL",
-                "userId INTEGER NOT NULL"
-            });
-            this->predictions = factory.create("predictions", {
-                "userId INTEGER NOT NULL",
-                "value INTEGER NOT NULL",
-                "FOREIGN KEY(userId) REFERENCES users(id)"
-            });
-        }
+ public:
+    std::shared_ptr<Table> users;
+    std::shared_ptr<Table> journals;
+    std::shared_ptr<Table> answers;
+    std::shared_ptr<Table> questions;
+    std::shared_ptr<Table> settings;
+    std::shared_ptr<Table> userdata;
+    std::shared_ptr<Table> predictions;
+    explicit Database(std::string path){
+        TableFactory factory{path};
+        this->users = factory.create("users", {
+            "username VARCHAR UNIQUE NOT NULL",
+            "password VARCHAR NOT NULL",
+            "userdataId INTEGER",
+            "state INTEGER NOT NULL",
+            "FOREIGN KEY(userdataId) REFERENCES userdata(id)"
+        });
+        this->journals = factory.create("journals", {
+            "comment varchar",
+            "userId INTEGER NOT NULL",
+            "FOREIGN KEY(userId) REFERENCES users(id)"
+        });
+        this->answers = factory.create("answers", {
+            "answer VARCHAR NOT NULL",
+            "journalId INTEGER NOT NULL",
+            "questionId INTEGER NOT NULL",
+            "FOREIGN KEY(journalId) REFERENCES journals(id)",
+            "FOREIGN KEY(questionId) REFERENCES questions(id)"
+        });
+        this->questions = factory.create("questions", {
+            "tags VARCHAR",
+            "type INTEGER NOT NULL",
+            "question VARCHAR NOT NULL"
+        });
+        this->settings = factory.create("settings", {
+            "key VARCHAR NOT NULL",
+            "value VARCHAR NOT NULL",
+            "userId INTEGER NOT NULL",
+            "FOREIGN KEY(userId) REFERENCES users(id)"
+        });
+        this->userdata = factory.create("userdata", {
+            "agegroup VARCHAR NOT NULL",
+            "occupation VARCHAR NOT NULL",
+            "userId INTEGER NOT NULL"
+        });
+        this->predictions = factory.create("predictions", {
+            "userId INTEGER NOT NULL",
+            "value INTEGER NOT NULL",
+            "FOREIGN KEY(userId) REFERENCES users(id)"
+        });
+    }
 };
--- include/TestTemplate.hpp
+++ include/TestTemplate.hpp
@@ -1,6 +1,9 @@
+#pragma once
+
 #include <iostream>
 #include <vector>
 #include <string>
+#include <tuple>
 
 #include <nlohmann/json.hpp>
 
@@ -19,20 +22,19 @@ This is a template test, it functions as an abstraction of running a test, and p
 */
 template<typename Functor>
 class Test {
-    protected:
-        std::vector<std::tuple<std::string, Functor>> tests;
-        virtual void init(){}
-    public:
-        void add_test(std::string name, Functor test){
-            std::tuple<std::string, Functor> t{name, test};
-            tests.push_back(t);
+ protected:
+    std::vector<std::tuple<std::string, Functor>> tests;
+    virtual void init() {}
+ public:
+    void add_test(std::string name, Functor test) {
+        std::tuple<std::string, Functor> t{name, test};
+        tests.push_back(t);
+    }
+    void run(){
+        for (auto [name, functor] : tests) {
+            std::cout << "......\tRunning test [" << name << "]" << std::endl;
+            init();
+            functor();
         }
-
-        void run(){
-            for(auto [name, functor] : tests){
-                std::cout << "......\tRunning test [" << name << "]" << std::endl;
-                init();
-                functor();
-            }
-        }
+    }
 };
