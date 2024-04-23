c200e43b9d09628d8b93667dee944afb5055b603
Taoshix <nicolaikh@live.dk>
2024-04-23 13:09:12 +0200

    format

--- include/Routes/Questions.hpp
+++ include/Routes/Questions.hpp
@@ -13,7 +13,7 @@ class Questions : public Route {
  public:
     void init() override {
         this->server->Get("/questions/defaults", [&](Request request, Response& response){
-            if(db->questions->get_where("tags", "default").size() == 0){
+            if (db->questions->get_where("tags", "default").size() == 0) {
                 json response_data;
                 response_data["error"] = "Somehow there are no default questions???";
                 return respond(&response, response_data, 500);
@@ -32,7 +32,7 @@ class Questions : public Route {
         });
         this->server->Get("/questions/get/:tag", [&](Request request, Response& response){
             auto tag = request.path_params["tag"];
-            if(db->questions->get_where("tags", tag).size() == 0){
+            if (db->questions->get_where("tags", tag).size() == 0) {
                 json response_data;
                 response_data["error"] = "No Questions With that Tag.";
                 return respond(&response, response_data, 400);
